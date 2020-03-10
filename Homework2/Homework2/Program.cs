3dc78c65643a7bf5418bc9e9f61b6de8d28710a7
darkNapis <nikolic.apis.darko@gmail.com>
2020-03-10 14:25:45 +0100

    darko

---
+++ Homework2/Homework2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            //string one = "Darko ";
+            //string two = "Nikolikj";
+            //string result = one + two;
+            //Console.WriteLine(result);
+
+            //string num1 = "9";
+            //string num2 = "3";
+            //int result = int.Parse(num1 + num2);
+            //Console.WriteLine(result);
+
+            // Exercise 2
+            //int num = 8;
+            //string str = "August";
+            //var result = num + (str);
+            //Console.WriteLine(result);
+
+            // Exercise 3
+            //var n = 102;
+            //var m = 5;
+            //var result = n / m;
+            //Console.WriteLine(result);
+
+            //Exercise 4
+
+            //Console.WriteLine("Enter a number:");
+
+            //var inputNumber = Console.ReadLine();
+            
+            //int numberValue;
+
+            //bool parsedNum = int.TryParse(inputNumber, out numberValue);
+            
+            //if(parsedNum)
+            //{
+            //    var oddEven = numberValue % 2 == 0 ? "EVEN" : "ODD";
+            //    Console.WriteLine(oddEven);
+            //} else
+            //{
+            //    Console.WriteLine("Invalid Input");
+            //    Console.ReadLine();
+            //}
+            }            
+        }
+    }
+
